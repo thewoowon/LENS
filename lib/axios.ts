8ee2760cfcb0c9126fe5c04c6cffdb85449749ff
thewoowon <thewoowon@naver.com>
2024-05-19 13:44:51 +0900

    feat: api 고정 및 최종 url

--- lib/axios.ts
+++ lib/axios.ts
@@ -4,8 +4,7 @@ import axios from "axios";
 const customAxios = axios.create({
   withCredentials: true, // CORS 요청 시 인증 정보를 전송하도록 설정
   // 기타 필요한 기본 설정 추가
-  baseURL:
-    "http://lens-server-load-balancer-486960209.ap-northeast-2.elb.amazonaws.com",
+  baseURL: "https://api.lensql.chat",
 });
 
 // 에러 처리
