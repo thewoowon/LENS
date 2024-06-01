52589a37b4c0fe5882cb140bdf5b608f588013c4
thewoowon <thewoowon@naver.com>
2024-06-01 21:09:04 +0900

    feat: axios custom erro exception 추가

--- lib/axios.ts
+++ lib/axios.ts
@@ -5,6 +5,11 @@ const customAxios = axios.create({
   withCredentials: true, // CORS 요청 시 인증 정보를 전송하도록 설정
   // 기타 필요한 기본 설정 추가
   baseURL: "https://api.lensql.chat",
+  headers: {
+    "Content-Type": "application/json",
+    // access_token: localStorage.getItem("accessToken") || "",
+    // refresh_token: localStorage.getItem("refreshToken") || "",
+  },
 });
 
 // 에러 처리
@@ -13,8 +18,11 @@ customAxios.interceptors.response.use(
     return response;
   },
   (error) => {
-    if (error.response.data.code == 950) {
-      window.location.replace("/auth/kakao");
+    // 로그아웃 처리하기
+    if (error.response?.status === 401) {
+      // 로그아웃 처리
+      // localStorage.removeItem("accessToken");
+      window.location.href = "/auth/login";
     }
     // 요청 실패 시 특별히 처리할 작업이 없으면 그대로 반환
     return Promise.reject(error);
