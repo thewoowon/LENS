601004e0e57e83eb644e07bb8f389f4257952dfa
thewoowon <thewoowon@naver.com>
2024-07-11 05:04:02 +0900

    feat: 회사 로고 컴포넌트

---
+++ components/Company/index.ts
@@ -0,0 +1,8 @@
+export { default as AliExpress } from "./AliExpress";
+export { default as Samsung } from "./Samsung";
+export { default as Google } from "./Google";
+export { default as Amazon } from "./Amazon";
+export { default as Netflix } from "./Netflix";
+export { default as Zerply } from "./Zerply";
+export { default as Microsoft } from "./Microsoft";
+export { default as Toyota } from "./Toyota";
