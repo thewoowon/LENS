9460fcbe348eeb463cb05c2d4cf9e6c08dc70771
thewoowon <thewoowon@naver.com>
2024-07-11 16:41:55 +0900

    feat: axios interceptor

--- lib/axios.ts
+++ lib/axios.ts
@@ -10,21 +10,76 @@ const customAxios = axios.create({
   },
 });
 
-// 에러 처리
+// Request interceptor to add access token to headers
+customAxios.interceptors.request.use(
+  (config) => {
+    const accessToken = localStorage.getItem("access_token");
+    if (accessToken) {
+      config.headers["Authorization"] = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
 customAxios.interceptors.response.use(
   (response) => {
     return response;
   },
-  (error) => {
-    // 로그아웃 처리하기
-    if (error.response?.status === 401) {
-      // 로그아웃 처리
-      // localStorage.removeItem("accessToken");
-      window.location.href = "/auth/login";
+  async (error) => {
+    const originalRequest = error.config;
+    const refreshToken = localStorage.getItem("refresh_token");
+
+    if (
+      error.response.status === 401 &&
+      refreshToken &&
+      !originalRequest._retry
+    ) {
+      originalRequest._retry = true;
+      try {
+        const response = await axios.post(
+          "https://api.lensql.chat/user/token/refresh",
+          {
+            token: refreshToken,
+          }
+        );
+
+        const { access_token } = response.data;
+        localStorage.setItem("access_token", access_token);
+
+        // Update the Authorization header and retry the original request
+        originalRequest.headers["Authorization"] = `Bearer ${access_token}`;
+        return customAxios(originalRequest);
+      } catch (refreshError) {
+        // Handle token refresh errors, e.g., logout user
+        console.error("Refresh token is invalid or expired", refreshError);
+        localStorage.removeItem("access_token");
+        localStorage.removeItem("refresh_token");
+        window.location.href = "/login";
+        return Promise.reject(refreshError);
+      }
     }
-    // 요청 실패 시 특별히 처리할 작업이 없으면 그대로 반환
     return Promise.reject(error);
   }
 );
+
+// // 에러 처리
+// customAxios.interceptors.response.use(
+//   (response) => {
+//     return response;
+//   },
+//   (error) => {
+//     // 로그아웃 처리하기
+//     if (error.response?.status === 401) {
+//       // 로그아웃 처리
+//       // localStorage.removeItem("accessToken");
+//       window.location.href = "/auth/login";
+//     }
+//     // 요청 실패 시 특별히 처리할 작업이 없으면 그대로 반환
+//     return Promise.reject(error);
+//   }
+// );
 
 export default customAxios;
