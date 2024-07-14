58eb8c30f2e99d70c45fa15c0c466dea9bb5b5b8
thewoowon <thewoowon@naver.com>
2024-07-15 08:18:24 +0900

    feat: main stream 영역 access param 작업

--- app/api/stream/route.ts
+++ app/api/stream/route.ts
@@ -15,6 +15,11 @@ async function fetchWithTimeout(url: string, options: any, timeout = 10000) {
 export async function POST(req: NextRequest) {
   const { prompt, sessionId, accessToken } = await req.json();
   try {
+
+    if (!accessToken) {
+      throw new Error("Access token is required");
+    }
+
     const response = await fetchWithTimeout(
       process.env.NEXT_PUBLIC_API_URL + "/v1/llm/execute_llm",
       {
@@ -30,6 +35,18 @@ export async function POST(req: NextRequest) {
     if (!response.body) {
       throw new Error("No response body");
     }
+
+    if (!sessionId) {
+      // 세션 ID가 없는 경우, 새로운 세션을 생성하고 리디렉션 응답을 반환
+      // 형식은 다음과 같음 {"redirect_path":"/chat/b020a6cc-b5fe-4f42-879c-c9731693dda0"}
+      const { redirect_path } = await response.json();
+      return new NextResponse(
+        JSON.stringify({ redirect_path }),
+        { status: 307 }
+      );
+    }
+
+    console.log("Response status:", response.status);
 
     const stream = new ReadableStream({
       start(controller) {
